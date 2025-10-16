6847b21344605ada77508775f76d0e2acb59fc54
Amr Hesham <42917363+Amr-H25@users.noreply.github.com>
2025-10-16 17:59:33 +0300

    Update run-action.sh

--- run-action.sh
+++ run-action.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
-aws s3 ls
+echo "Listing EC2 instances..."
+aws ec2 describe-instances --region us-east-1 --query "Reservations[*].Instances[*].{ID:InstanceId,State:State.Name}" --output table
