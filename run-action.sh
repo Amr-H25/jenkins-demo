8594a43582ff2ae2c3410d637bc305f0c21a0f2f
Amr Hesham <42917363+Amr-H25@users.noreply.github.com>
2025-10-16 18:01:21 +0300

    Change AWS region from us-east-1 to eu-west-1

--- run-action.sh
+++ run-action.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 echo "Listing EC2 instances..."
-aws ec2 describe-instances --region us-east-1 --query "Reservations[*].Instances[*].{ID:InstanceId,State:State.Name}" --output table
+aws ec2 describe-instances --region eu-west-1 --query "Reservations[*].Instances[*].{ID:InstanceId,State:State.Name}" --output table
